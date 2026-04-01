a20f5537ea7ba324c97510b283fd607e78555603
cleiber-mata <cleiber.mata@gmail.com>
2026-04-01 00:51:39 -0300

    sql

---
+++ idade_classificacao.php
@@ -0,0 +1,18 @@
+<?php
+/*
+5. Classificação de idade
+Receba a idade de uma pessoa e classifique:
+
+0–12 → Criança
+13–17 → Adolescente
+18–59 → Adulto
+60+ → Idoso5. Classificação de idade
+Receba a idade de uma pessoa e classifique:
+
+0–12 → Criança
+13–17 → Adolescente
+18–59 → Adulto
+60+ → Idoso
+*/
+?>
+
