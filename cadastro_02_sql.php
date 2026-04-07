a37dbb65a18f09d6d5efadcb4660d1aeabdbe76e
cleiber-mata <cleiber.mata@gmail.com>
2026-04-07 01:14:56 -0300

    add

---
+++ cadastro_02_sql.php
@@ -0,0 +1,44 @@
+<form method="POST">
+    <br><br>
+    <label for="prefixo_mt">prefixo_mt:</label>
+    <input type="text" id="prefixo_mt" name="prefixo_mt">
+    <br><br>
+
+    <label for="modelo_mt">modelo_mt:</label>
+    <input type="text" id="modelo_mt" name="modelo_mt">
+    <br><br>
+
+    <label for="placa_mt">placa_mt:</label>
+    <input type="text" id="placa_mt" name="placa_mt">
+    <br><br>
+
+
+    <button type="submit">Enviar</button>
+
+</form>
+
+<?php
+$conn = new mysqli("localhost", "root", "", "cadastro_php");
+
+if ($conn->connect_error) {
+    die("Erro de conexão: " . $conn->connect_error);
+}
+
+$prefixo_mt = $_POST["prefixo_mt"] ?? "";
+$modelo_mt = $_POST["modelo_mt"] ?? "";
+$placa_mt = $_POST["placa_mt"] ?? "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $sql = "INSERT INTO motos (prefixo_mt, modelo_mt, placa_mt)
+            VALUES ('$prefixo_mt', '$modelo_mt', '$placa_mt')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "<br>Dados salvos com sucesso!";
+    } else {
+        echo "<br>Erro ao salvar: " . $conn->error;
+    }
+}
+
+$conn->close();
+?>
--- cadastro_sql.php
+++ cadastro_sql.php
@@ -1,15 +1,19 @@
 <form method="POST">
     <br><br>
-    <label for="nome">Nome:</label>
-    <input type="text" id="nome" name="nome">
+    <label for="nome_pol">nome_pol:</label>
+    <input type="text" id="nome_pol" name="nome_pol">
     <br><br>
     
-    <label for="sobrenome">Sobrenome:</label>
-    <input type="text" id="sobrenome" name="sobrenome">
+    <label for="sobrenome_pol">sobrenome_pol:</label>
+    <input type="text" id="sobrenome_pol" name="sobrenome_pol">
     <br><br>
     
-    <label for="dn">Data nascimento:</label>
-    <input type="date" id="dn" name="DN">
+    <label for="matricula">matricula:</label>
+    <input type="varchar" id="matricula" name="matricula">
+    <br><br>
+
+    <label for="celular_pol">celular_pol:</label>
+    <input type="int" id="celular_pol" name="celular_pol">
 
     <button type="submit">Enviar</button>
 
@@ -22,14 +26,15 @@ if ($conn->connect_error) {
     die("Erro de conexão: " . $conn->connect_error);
 }
 
-$nome = $_POST["nome"] ?? "";
-$sobrenome = $_POST["sobrenome"] ?? "";
-$data_nascimento = $_POST["DN"] ?? "";
+$nome_pol = $_POST["nome_pol"] ?? "";
+$sobrenome_pol = $_POST["sobrenome_pol"] ?? "";
+$matricula = $_POST["matricula"] ?? "";
+$celular_pol = $_POST["celular_pol"] ?? "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $sql = "INSERT INTO pessoas (nome, sobrenome, data_nascimento)
-            VALUES ('$nome', '$sobrenome', '$data_nascimento')";
+    $sql = "INSERT INTO policiais (nome_pol, sobrenome_pol, matricula, celular_pol)
+            VALUES ('$nome_pol', '$sobrenome_pol', '$matricula', '$celular_pol')";
 
     if ($conn->query($sql) === TRUE) {
         echo "<br>Dados salvos com sucesso!";
